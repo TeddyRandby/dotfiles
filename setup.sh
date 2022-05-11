201093cbe457e7552abe32d88ce80a13a6fc442b
ted <tedrandby@gmail.com>
2022-05-11 09:26:02 -0400

    Moved things around

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!usr/bin/bash
+
+# -- Install dependencies --
+
+# Pip
+sudo apt-get install python3-pip
+# Node and npm
+sudo apt-get install nodejs
+# Cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# -- Install the latest nvim binary --
+bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
+
+# -- Install lvim --
+LV_BRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
