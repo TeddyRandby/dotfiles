b40f9389e0a162ce1767eeab8b2188e45b64f2d9
Teddy Randby <42707054+TeddyRandby@users.noreply.github.com>
2022-09-13 20:54:37 +0000

    Add lazygit

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt upgrade -y
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo apt-get update
-sudo apt-get install ranger neovim -y
+sudo apt-get install ranger lazygit neovim -y
 
 git clone https://github.com/TeddyRandby/nvim-config.git ~/.config/nvim
 
