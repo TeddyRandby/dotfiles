2f247be7e8880f98b445da5c43e2483c3d582365
Teddy Randby <42707054+TeddyRandby@users.noreply.github.com>
2022-09-13 18:28:10 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt upgrade -y
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo apt-get update
-sudo apt-get install neovim -y
+sudo apt-get install ranger neovim -y
 
 git clone https://github.com/TeddyRandby/nvim-config.git ~/.config/nvim
 
