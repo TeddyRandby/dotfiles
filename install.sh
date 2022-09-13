ed2668eab59dc4cdbe1670d3bb6017a88a1eff6d
Teddy Randby <42707054+TeddyRandby@users.noreply.github.com>
2022-09-13 20:57:03 +0000

    Ooops there is no lazygit package

--- install.sh
+++ install.sh
@@ -5,7 +5,13 @@ sudo apt upgrade -y
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo apt-get update
-sudo apt-get install ranger lazygit neovim -y
+sudo apt-get install ranger neovim -y
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
+
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+
+sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
 
 git clone https://github.com/TeddyRandby/nvim-config.git ~/.config/nvim
 
