2669daeec1a758933a087f70deb1359af83db913
Teddy Randby <42707054+TeddyRandby@users.noreply.github.com>
2022-09-13 16:03:41 +0000

    Update command to work

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ sudo apt-get install neovim -y
 git clone https://github.com/TeddyRandby/nvim-config.git ~/.config/nvim
 git clone https://github.com/TeddyRandby/tmux-config.git ~/.config/tmux
 
-echo "export TERM=xterm-256colors" | ~/.profile
+echo "export TERM=xterm-256color" >> ~/.profile
